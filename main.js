c7d8960602a17f67f999097b3dfa04b0c4a93b34
Parish Neighbors <pn8bors@gmail.com>
2022-02-02 13:50:12 -0600

    Fourth commit

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ import input from 'input';
 
 async function main() {
     const drivingAge = 16;
-    let yourAge = await input.text('What is your age?')
     while (true) {
+        let yourAge = await input.text('What is your age?')
         if (yourAge < drivingAge) {
             console.log('You are too young to drive.');
             console.log(`You must wait ${drivingAge - yourAge} years.`)
-        continue
+        return
         }
 
     const hasLicense = await input.confirm('Do you have your license?');
@@ -15,6 +15,11 @@ async function main() {
             console.log('You can drive!');
         } else {
             console.log('You must get your license')
+        }
+        
+        const goOn = await input.confirm('Would you like to continue?');
+        if (!goOn) {
+            break
         }
     }
 }
