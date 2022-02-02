8f34f83eb2ecc29f29efec4c4807b4e6fce117d0
Parish Neighbors <pn8bors@gmail.com>
2022-02-02 10:49:38 -0600

    Second commit

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ async function main() {
     let yourAge = await input.text('What is your age?')
 
     if (yourAge < drivingAge) {
-        console.log('You are too young to drive');
+        console.log('You are too young to drive.');
+        console.log(`You must wait ${drivingAge - yourAge} years.`)
+    return
     }
 
     const hasLicense = await input.confirm('Do you have your license?');
