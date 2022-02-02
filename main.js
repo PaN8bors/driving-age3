1644e0b96e89e4367e05e0a8cddc9ce6b12a2ac9
Parish Neighbors <pn8bors@gmail.com>
2022-02-02 09:24:37 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import input from 'input';
+
+async function main() {
+    const drivingAge = 16;
+    let yourAge = await input.text('What is your age?')
+
+    if (yourAge < drivingAge) {
+        console.log('You are too young to drive');
+    }
+
+    const hasLicense = await input.confirm('Do you have your license?');
+    
+    if (yourAge >= drivingAge && hasLicense) {
+        console.log('You can drive!');
+    } else {
+        console.log('You must get your license')
+    }
+}
+
+main();
